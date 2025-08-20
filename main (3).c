33ad04b84acae6e5fcdb019ec90995daeace253f
isabelazv-spect <bebelazv@hotmail.com>
2025-08-20 11:44:56 -0300

    Update main (3).c

--- main (3).c
+++ main (3).c
@@ -1,23 +1,23 @@
 #include <stdio.h>
- int main()
+int main()
 {
-int num1 , num2 , anx;
+	int num1, num2, anx;
 
-printf("Digite o numero de alunos: ");
-scanf("%d", &num1);
+	printf("Digite o numero de alunos: ");
+	scanf("%d", &num1);
 
-printf("Digite o numero de monitores");
-scanf("%d", &num2);
+	printf("Digite o numero de monitores: ");
+	scanf("%d", &num2);
 
-anx=num1+num2;
+	anx=num1+num2;
 
-if (anx <= 50)
-{
-    printf("Possivel");
+	if (anx <= 50)
+	{
+		printf("Possivel");
+	}
+	else
+	{
+		printf("Nao e possivel");
+	}
+	return 0;
 }
-else
-{
-    printf("Nao e possivel");
-}
- return 0;
- }
