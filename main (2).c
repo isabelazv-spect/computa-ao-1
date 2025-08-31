d8f62248c52ca8b3a226e6a575ddcd97fddd3d19
isabelazv-spect <bebelazv@hotmail.com>
2025-08-31 18:55:10 -0300

    Add files via upload

--- main (2).c
+++ main (2).c
@@ -1,20 +1,32 @@
 #include <stdio.h>
 int main()
 {
-int i;
-float f;
-char c;
+	float num1, num2, num3;
 
-printf("Digite um caractere: ");
-scanf("%c", &c);
+	printf("Digite um dos lados do triangulo: ");
+	scanf("%f", &num1);
 
-printf("Digite um valor inteiro: ");
-scanf("%d", &i);
+	printf("Digite o segundo lado do triangulo: ");
+	scanf("%f", &num2);
 
-printf("Entre com um numero de ponto flutuante (valor nao inteiro): ");
-scanf("%f", &f);
+	printf("Digite o terceiro lado do triangulo: ");
+	scanf("%f", &num3);
 
-printf ("%c,%d,%f", c,i,f );
-
-return 0;
+	if(num1 >= num2+num3 || num2 >= num3+num1 || num3 >= num2+num1)
+	{
+		printf("Nao forma um triangulo! ");
+	}
+	else if(num1 == num2 && num2 == num3)
+	{
+		printf("Forma um triangulo equilatero ");
+	}
+	else if(num1 != num2 && num2 != num3)
+	{
+		printf("Forma um triangulo escaleno ");
+	}
+	else if(num1 == num2 || num1 == num3 || num2 == num3)
+	{
+	    printf("Forma triangulo isosceles ");
+	}
+		return 0;
 }
