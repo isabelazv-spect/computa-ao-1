ecccdb30ed341f1552aa3906b7fdcfab7078adb3
isabelazv-spect <bebelazv@hotmail.com>
2025-09-02 18:59:06 -0300

    Add files via upload

--- main (6).c
+++ main (6).c
@@ -1,46 +1,36 @@
 #include <stdio.h>
-#include <math.h>
 
 int main()
 {
-	float num1, num2, num3, media1, media2, media3, media4;
-	char opcao;
-	printf("escolha a opcao (A, P, G, H):");
-	scanf("%c", &opcao);
+    float num1, num2, num3, num4, aux1, aux2;
+    
+    printf("Digite o preço do litro do  alcool: ");
+    scanf("%f", &num1);
+    
+    printf("Digite o preço do litro da gasolina: ");
+    scanf("%f", &num2);
+    
+    printf("Digite quantos km por litro o carro realiza com gasolina: ");
+    scanf("%f", &num3);
+    
+    printf("Digite quantos km por litro o crro realiza com alcool: ");
+    scanf("%f", &num4);
+    
+    aux1 = num1 / num4;
+    aux2 = num2 / num3;
+    
+    if(aux1>aux2)
+    {
+        printf("O alcool vale mais apena! ");
+    }
+    else if(aux2>aux1)
+    {
+        printf("A gasolina vale mais apena! ");
+    }
+    else
+    {
+        printf("Os dois sao equivalentes! ");
+    }
 
-	printf("Digite um numero: ");
-	scanf("%f", &num1);
-
-	printf("Digite um numero: ");
-	scanf("%f", &num2);
-
-	printf("Digite um numero: ");
-	scanf("%f", &num3);
-
-	if(opcao== 'A' || opcao == 'a')
-
-	{
-		media1 = (num1+num2+num3) / 3;
-		printf("Media aritmetica = ""%f", media1);
-	}
-	else if(opcao == 'P' || opcao == 'p')
-	{
-		media2 = (num1*1+num2*2+num3*3) / 6;
-		printf("Media ponderada = ""%f", media2);
-	}
-	else if(opcao == 'G' || opcao == 'g')
-	{
-		media3 = pow((num1*num2*num3),3);
-		printf("Media geometrica = ""%f", media3);
-	}
-	else if(opcao == 'H' || opcao == 'h')
-	{
-		media4 = (3 / (1/num1)+(1/num2)+(1/num3));
-		printf("Media harmonica = ""%f", media4);
-	}
-	else
-	{
-		printf("Erro");
-	}
-	return 0;
+    return 0;
 }
