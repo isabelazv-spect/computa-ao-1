ad8a3398a641ae30e48445aac5d6b6613bb481df
isabelazv-spect <bebelazv@hotmail.com>
2025-10-01 09:22:18 -0300

    Add files via upload

---
+++ main (13).c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int ehBissexto(int ano)
+
+{
+    
+	if ((ano % 4 == 0 && ano % 100 != 0) || (ano % 400 == 0))
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+int main ()
+
+{
+	int ano;
+
+	printf("Digite um ano: ");
+	scanf("%d", &ano);
+
+	if (ehBissexto(ano))
+	{
+		printf("O ano e bissexto ! ");
+	}
+	else
+	{
+		printf("Nao e bissexto ! ");
+	}
+
+return 0;
+}
