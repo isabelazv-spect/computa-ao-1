bf47fb7e75aa1e1f1bc70f1a9aefb607dcb08484
isabelazv-spect <bebelazv@hotmail.com>
2025-11-30 22:08:46 -0300

    Add files via upload

---
+++ main (21).c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    int num1;
+    int j =1 , i = 1;
+    
+    printf("digite um numero: ");
+    scanf("%d", &num1);
+    
+    printf(" \n");
+    
+    for(i = 1; i <= num1; i++ )
+    {
+        printf("%d", i);
+        
+        for(j = 1; j <= i; j++)
+        {
+            printf("%d", i * j);
+        }
+        
+        printf("\n");
+    }
+
+    return 0;
+}
